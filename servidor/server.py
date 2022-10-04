65d827f964614fe2c7e1929ac02474c6e85a4186
nicolas <sn.rodriguezc@uniandes.edu.co>
2022-10-03 20:06:00 -0500

    fix file transfer

--- servidor/server.py
+++ servidor/server.py
@@ -1,8 +1,9 @@
-import datetime, logging, socket, sys, threading, os, hashlib, time
+import datetime, logging, socket, sys, threading, os, hashlib, time, tqdm
 
 HOST = '192.168.20.48'
 PORT = 444
 FORMAT = 'utf-8'
+BUFFER_SIZE = 4096
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Socket created')
@@ -35,38 +36,47 @@ def getHashDigest(file):
 #Function for handling connections. This will be used to create threads
 def clientthread(conn, fName):
 
-    # Lectura de archivo
-    file = open(fName, "r")
-    fileStats = os.stat(fName)
-    fileSizeMB = fileStats.st_size / (1024 * 1024)
-    data = file.read()
-    
     # Identificacion del cliente de la conexion
     idClient = conn.recv(1024)
 
-    # Calcular hash para el archivo
-    hashValue = getHashDigest(file)
+    # start sending the file
+    fileSizeBytes = os.path.getsize(fName)
+    progress = tqdm.tqdm(range(fileSizeBytes), f"Sending {fName}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(fName, "rb") as f:
 
-    #Envio de archivo
-    start = time.time()
-    conn.send(data.encode(FORMAT))
-    logging.info('SENT {} WITH SIZE {}MB TO CLIENT #{}'.format(fName, fileSizeMB, idClient))
+        # Calcular hash para el archivo
+        hashValue = getHashDigest(f)
+        #Envio de archivo
+        start = time.time()
+        while True:
+            # read the bytes from the file
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                # file transmitting is done
+                break
+            # we use sendall to assure transimission in 
+            # busy networks
+            s.sendall(bytes_read)
+            # update the progress bar
+            progress.update(len(bytes_read))
 
-    exito = conn.recv(1024)
-    if not exito:
-        logging.error('FROM CLIENT #{}: File transfer failed')
-        conn.close()
+        logging.info('SENT {} WITH SIZE {}MB TO CLIENT #{}'.format(fName, fileSizeBytes/ (1024 * 1024), idClient))
 
-    logging.info('FROM CLIENT #{}: {}'.format(idClient, exito))
-    
-    end = time.time()
-    
-    #Calculo de tiempo de transferencia
-    logging.info('TRANSFER TIME FOR CLIENT #{}: {}'.format(idClient, start-end))
+        exito = conn.recv(1024)
+        if not exito:
+            logging.error('FROM CLIENT #{}: File transfer failed')
+            conn.close()
 
-    #Envio de hash
-    conn.send(hashValue.encode(FORMAT))
-    logging.info('SENT HASH {} TO CLIENT #{}'.format(hashValue, idClient))
+        logging.info('FROM CLIENT #{}: {}'.format(idClient, exito))
+        
+        end = time.time()
+
+        #Calculo de tiempo de transferencia
+        logging.info('TRANSFER TIME FOR CLIENT #{}: {}'.format(idClient, end-start))
+
+        #Envio de hash
+        conn.send(hashValue.encode(FORMAT))
+        logging.info('SENT HASH {} TO CLIENT #{}'.format(hashValue, idClient))
 
     conn.close()
 
@@ -77,10 +87,11 @@ fileName = conn.recv(1024).decode(FORMAT)
 nClients = int.from_bytes(conn.recv(1024), "big")
 #conn.close()
 
+format = "%(asctime)s: %(message)s"
 now = datetime.datetime.now()
 
 logFileName = "{}-{}-{}-{}-{}-{}.log".format(now.year, now.month, now.day, now.hour, now.minute, now.second)
-logging.basicConfig(filename=logFileName, level=logging.INFO)
+logging.basicConfig(format=format, datefmt="%H:%M:%S", filename=logFileName, level=logging.INFO)
 
 tList = []
 #now keep talking with the client
